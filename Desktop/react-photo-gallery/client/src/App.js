b9b763d56180a778aba3e2e723fc6ed7d4566f49
artgirl23 <68173251+artgirl23@users.noreply.github.com>
2021-10-07 19:28:08 -0700

    App so far

---
+++ Desktop/react-photo-gallery/client/src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import images from './api-mock.json';
+import './App.css';
+
+const  App = () => {
+  const [imageList, setImageList] = useState(images.resources);
+  return (<div className="image-grid">
+    {imageList.map((image)=> (<img src={image.url} alt={image.public_id}></img>
+    ))}
+  </div>
+  );
+};
+
+export default App;
+
