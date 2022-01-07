dbab7f6a526548d668f258eb87a5072c63781bdc
artgirl23 <68173251+artgirl23@users.noreply.github.com>
2022-01-07 14:41:32 -0800

    Currency Pt One

---
+++ Desktop/exchange-rate-app/src/App.js
@@ -0,0 +1,22 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Layout from './Layout';
+import Home from './Home';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <Router>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route render={() => <h1>404 Not found</h1>} />
+        </Switch>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Desktop/exchange-rate-app/src/index.js
@@ -0,0 +1,11 @@
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
